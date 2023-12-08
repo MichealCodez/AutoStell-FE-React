0a423e88bee0ccfbb2a81fe974c37477e53cdde3
MichealDavid1 <madtechnow@gmail.com>
2023-12-08 23:12:19 +0100

    update

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -3,8 +3,8 @@ import '../styles/Footer.css'
 function Footer(){
     return(
         <div className="footer">
-            <p>Autostell By <a href='https://f7team.com' style='color: green;'>F7</a></p>
-            <p>&copy;2023 <a href='https://f7team.com' style='color: green;'>F7</a>. All Rights Reserved</p>
+            <p>Autostell By <a href='https://f7team.com' className='f7-link'>F7</a></p>
+            <p>&copy;2023 <a href='https://f7team.com' className='f7-link'>F7</a>. All Rights Reserved</p>
         </div>
     )
 }
