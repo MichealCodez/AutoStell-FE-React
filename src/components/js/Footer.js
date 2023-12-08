c7bb8214e0949a5c7ec9afe7d0458fee241eb60b
MichealDavid1 <madtechnow@gmail.com>
2023-12-08 22:46:52 +0100

    add link

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -3,8 +3,8 @@ import '../styles/Footer.css'
 function Footer(){
     return(
         <div className="footer">
-            <p>Autostell By F7</p>
-            <p>&copy;2023 f7. All Rights Reserved</p>
+            <p>Autostell By <a href='https://f7team.com'>F7</a></p>
+            <p>&copy;2023 <a href='https://f7team.com'>F7</a>. All Rights Reserved</p>
         </div>
     )
 }
