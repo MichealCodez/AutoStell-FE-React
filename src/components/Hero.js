f684fc1c1f29bba5c701f9f41494403382c6c206
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-08 23:24:07 +0100

    update pull and fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,35 @@
-import React, { useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import Header from './Header';
 import './Hero.css';
 import topBackImage from '../images/top-background-logo.png';
+import image1 from '../images/cars/0.jpg'
 
 export default function Hero({ onClickJoin }) {
 
-    // const [rotationAngle, setRotationAngle] = useState(0);
+    const [rotationAngle, setRotationAngle] = useState(0);
 
-    // useEffect(() => {
-    //     const intervalId = setInterval(() => {
-    //         setRotationAngle((prevAngle) => (prevAngle + 15) % 360);
-    //     }, 500);
-    //     return () => clearInterval(intervalId);
-    // }, []);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setRotationAngle((prevAngle) => (prevAngle + 5) % 80);
+          }, 300);
+        return () => clearInterval(intervalId);
+    }, []);
 
-    // const carStyle = {
-    //     transform: `rotate(${rotationAngle}deg)`,
-    // };
+    const carStyle = {
+        transition: 'transform 0.5s ease-in-out',
+    };
 
-
+    const image = require(`../images/cars/${rotationAngle}.jpg`)
     return (
         <div className='Hero'>
             <Header onClickJoin={onClickJoin} />
             <div className='hero-image-background'>
-                <img src={topBackImage} alt='CAR' />
-                {/* <img
-                    // src={`.car_${rotationAngle}.png`}
-                    src={`../images/car1.png`}
+                <img
+                    src={image}
                     alt="Rotating Car"
                     className="rotating-car"
                     style={carStyle}
-                /> */}
+                />
             </div>
         </div>
     )
