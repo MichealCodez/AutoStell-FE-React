42e9a6fe54ce85e85ead2229a27e8d52512b3c87
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-08 21:39:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -28,14 +28,14 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
+        <Hero onClickJoin={toggleOpenBottomSheet} />
         <div className='page-content'>
-          <Hero onClickJoin={toggleOpenBottomSheet} />
           <div className='top-text'>
             <h2>Introduction To Autostell</h2>
             <p>In today's fast-paced world, buying and selling cars often involves cumbersome processes, lack of transparency, and limited access to reliable automobile services. Additionally, car owners frequently face challenges in finding skilled repairers and engineers to address their specific automotive issues. There is a pressing need for a comprehensive online platform that seamlessly connects buyers and sellers, while also providing a convenient and trustworthy avenue for individuals to access qualified automobile repairers and engineers. Autostell has prioritize user experience, security, and efficient communication channels, ensuring a hassle-free experience for all parties involved in the automobile trading and repair processes. Addressing these pain points will not only enhance the overall car trading experience but also foster a community where automotive enthusiasts can connect, share knowledge, and find tailored solutions for their automobile-related needs</p>
           </div>
           <div style={{
-            marginInline: 'auto'
+            width: '100%'
           }}>
             {contentData.map((item, index) => {
               if (index === 4) {
