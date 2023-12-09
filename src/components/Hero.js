84b5b19eca0d9ffd7d36fb8276fc0e722614d82e
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-09 16:18:29 +0100

    add rotating car at the top

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,38 +1,53 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import './Hero.css';
-import topBackImage from '../images/top-background-logo.png';
-import image1 from '../images/cars/0.jpg'
+import topBackImage from '../images/footer-card-image.png';
 
 export default function Hero({ onClickJoin }) {
-
-    const [rotationAngle, setRotationAngle] = useState(0);
+    const [rotationAngle, setRotationAngle] = useState(1);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setRotationAngle((prevAngle) => (prevAngle + 5) % 80);
-          }, 300);
+            setRotationAngle((prevAngle) => {
+                if (prevAngle === 72) {
+                    return 1;
+                }
+                else {
+                    return prevAngle + 1;
+                }
+            });
+        }, 50);
         return () => clearInterval(intervalId);
     }, []);
 
     const carStyle = {
-        transition: 'transform 0.5s ease-in-out',
+        transition: 'all 0.5s ease-in-out',
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0'
     };
 
-    const image = require(`../images/cars/${rotationAngle}.jpg`)
+    const image = require(`../images/rotating-cars/${rotationAngle}.png`);
+
     return (
         <div className='Hero'>
             <Header onClickJoin={onClickJoin} />
             <div className='hero-image-background'>
-                <img
-                    src={image}
-                    alt="Rotating Car"
-                    className="rotating-car"
-                    style={carStyle}
-                />
+                <div style={{
+                    position: 'relative'
+                }}>
+                    <img src={topBackImage} alt='backImage' />
+                    <img
+                        src={image}
+                        alt="Rotating Car"
+                        className="rotating-car"
+                        style={carStyle}
+                    />
+                </div>
+
             </div>
         </div>
-    )
+    );
 }
-
-
