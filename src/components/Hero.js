b2c2ad58d76bd641dac05650abb4dd7911598b37
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-14 13:45:28 +0100

    fix responsiveness in multi-platform

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,41 +5,41 @@ import "./Hero.css";
 import topBackImage from "../images/footer-card-image.png";
 
 export default function Hero({ onClickJoin }) {
-  // const [rotationAngle, setRotationAngle] = useState(1);
-  // const [images, setImages] = useState([]);
+  const [rotationAngle, setRotationAngle] = useState(1);
+  const [images, setImages] = useState([]);
 
-  // useEffect(() => {
-  //   const preloadImages = async () => {
-  //     const imageArray = Array.from({ length: 72 }, (_, index) => {
-  //       const img = new Image();
-  //       img.src = require(`../images/rotating-cars/${index + 1}.png`);
-  //       return img;
-  //     });
-  //     await Promise.all(imageArray.map((img) => img.decode()));
-  //     setImages(imageArray);
-  //   };
+  useEffect(() => {
+    const preloadImages = async () => {
+      const imageArray = Array.from({ length: 72 }, (_, index) => {
+        const img = new Image();
+        img.src = require(`../images/rotating-cars/${index + 1}.png`);
+        return img;
+      });
+      await Promise.all(imageArray.map((img) => img.decode()));
+      setImages(imageArray);
+    };
 
-  //   preloadImages();
-  // }, []);
+    preloadImages();
+  }, []);
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setRotationAngle((prevAngle) => (prevAngle === 72 ? 1 : prevAngle + 1));
-  //   }, 100);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRotationAngle((prevAngle) => (prevAngle === 72 ? 1 : prevAngle + 1));
+    }, 100);
 
-  //   return () => clearInterval(intervalId);
-  // }, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="Hero">
       <Header onClickJoin={onClickJoin} />
       <div className="hero-image-background">
           <img src={topBackImage} alt="backImage" className="static-image" />
-          {/* <img
+          <img
             src={images.length ? images[rotationAngle - 1].src : ""}
             alt="Rotating Car"
             className="rotating-car"
-          /> */}
+          />
       </div>
     </div>
   );
