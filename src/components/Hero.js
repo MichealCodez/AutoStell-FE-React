abb2017d2f4cedc7c924ae6154c6f588adb258bd
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-14 09:54:17 +0100

    fix responsiveness in multi-platform

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "./Header";
 import "./Hero.css";
 import topBackImage from "../images/footer-card-image.png";
