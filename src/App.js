fe206a24f9604e99be8d3adae92b6cb3b21d95b5
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-12-15 21:54:20 +0100

    resize card-container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-// import CardLeft from './components/CardLeft';
-// import CardRight from './components/CardRight';
+import CardLeft from './components/CardLeft';
+import CardRight from './components/CardRight';
 import Hero from './components/Hero';
-// import contentData from './content.json';
+import contentData from './content.json';
 import './App.css';
 import Footer from './components/js/Footer';
 import FooterCard from './components/js/FooterCard';
-// import CenterCard from './components/js/CenterCard';
+import CenterCard from './components/js/CenterCard';
 import BottomSheet from './components/js/BottomSheet'
 
 function App() {
@@ -30,12 +30,12 @@ function App() {
       <div className='container'>
         <Hero onClickJoin={toggleOpenBottomSheet} />
         <div className='page-content'>
-          <div className='top-text'>
+          {/* <div className='top-text'>
             <h2>Why Autostell?</h2>
             <p>Being a car enthusiast can be challenging at times, with difficulties in fixing automobile issues and selling cars. Autostell recognizes these issues and aims to provide a unified platform to address them. Think of it as a Stack Overflow for mechanics and a seamless marketplace for buying and selling cars. Autostell is committed to offering general solutions for various automobile problems.</p>
-          </div>
+          </div> */}
           <div className='card-container'>
-            {/* {contentData.map((item, index) => {
+            {contentData.map((item, index) => {
               if (index === 4) {
                 return (
                   <>
@@ -51,7 +51,7 @@ function App() {
                 return <CardLeft key={index} content={item} 
                 onClickJoin={toggleOpenBottomSheet} />
               }
-            })} */}
+            })}
 
           </div>
         </div>
