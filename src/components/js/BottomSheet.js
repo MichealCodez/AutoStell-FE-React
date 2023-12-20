1b2f9454853f5ceb8e9e2a4de8294b9f6a520868
MichealDavid1 <madtechnow@gmail.com>
2023-12-20 18:44:11 +0100

    Changed APIUrl and button message

--- src/components/js/BottomSheet.js
+++ src/components/js/BottomSheet.js
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import closeBtn from '../../images/close.svg';
-import '../styles/BottomSheet.css';
-import '../styles/WaitListComponent.css';
-import CustomDropDown from './CustomDropDown';
+import React, { useEffect, useState } from "react";
+import closeBtn from "../../images/close.svg";
+import "../styles/BottomSheet.css";
+import "../styles/WaitListComponent.css";
+import CustomDropDown from "./CustomDropDown";
 
 const BottomSheet = ({ isOpen, toggleCloseBottomSheet, toggleBottomSheet }) => {
-  const roles = ['Sell Cars', 'Buy Cars', 'Blog About Cars', 'Get A Repairer', 'Be A Repairer', 'Others'];
-  const apiUrl = 'https://f7team.vercel.app/api/autostells/';
-  const [email, setEmail] = useState('');
+  const roles = [
+    "Sell Cars",
+    "Buy Cars",
+    "Blog About Cars",
+    "Get A Repairer",
+    "Be A Repairer",
+    "Others",
+  ];
+  const apiUrlPost = "https://f7team.vercel.app/api/autostells/join_waitlist/";
+  const apiUrlGet = "https://f7team.vercel.app/api/autostells/";
+  const [email, setEmail] = useState("");
   const [role, setRole] = useState(null);
   const [data, setData] = useState(null);
   const [sending, setSending] = useState(false);
@@ -15,11 +23,11 @@ const BottomSheet = ({ isOpen, toggleCloseBottomSheet, toggleBottomSheet }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrlGet);
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
-        console.error('Error during GET request:', error);
+        console.error("Error during GET request:", error);
       }
     };
 
@@ -37,10 +45,10 @@ const BottomSheet = ({ isOpen, toggleCloseBottomSheet, toggleBottomSheet }) => {
 
     try {
       // Make POST request
-      const response = await fetch(apiUrl, {
-        method: 'POST',
+      const response = await fetch(apiUrlPost, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(requestData),
       });
@@ -51,41 +59,51 @@ const BottomSheet = ({ isOpen, toggleCloseBottomSheet, toggleBottomSheet }) => {
       if (response.ok) {
         const fetchData = async () => {
           try {
-            const response = await fetch(apiUrl);
+            const response = await fetch(apiUrlGet);
             const jsonData = await response.json();
             setData(jsonData);
           } catch (error) {
-            console.error('Error during GET request:', error);
+            console.error("Error during GET request:", error);
           }
         };
 
         fetchData();
       }
-
     } catch (error) {
-      console.error('Error during POST request:', error);
+      console.error("Error during POST request:", error);
     }
 
     setSending(false);
   };
 
   return (
-    <div className={`bottom-sheet ${isOpen ? 'open' : ''}`}>
+    <div className={`bottom-sheet ${isOpen ? "open" : ""}`}>
       <div className="wait-list-drawer">
         <div className="head">
           <p onClick={toggleBottomSheet}>Waitlist: {data ? data.length : 0}</p>
           {isOpen ? (
-            <img src={closeBtn} alt="close-btn" onClick={toggleCloseBottomSheet} />
+            <img
+              src={closeBtn}
+              alt="close-btn"
+              onClick={toggleCloseBottomSheet}
+            />
           ) : (
-            <p onClick={toggleBottomSheet} className='head-button'>Join Waitlist</p>
+            <p onClick={toggleBottomSheet} className="head-button">
+              Join Waitlist
+            </p>
           )}
         </div>
-        <div className='content'>
+        <div className="content">
           <CustomDropDown options={roles} onChangeRole={setRole} />
           <label>Email Address:</label>
-          <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} disabled={sending} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={sending}
+          />
           <button onClick={handleJoinWaitlist} disabled={sending}>
-            {sending ? 'Sending' : 'Join Waitlist'}
+            {sending ? "Joining..." : "Join Waitlist"}
           </button>
         </div>
       </div>
